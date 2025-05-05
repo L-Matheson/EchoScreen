99146c6fd240cf80214bbbdea76b1b5d6b3af62e
lmatheson2026 <144724348+lmatheson2026@users.noreply.github.com>
2025-05-05 18:41:57 -0400

    Node mods

---
+++ EchoScreenFrontend/src/components/EchoSideBar.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup>
+import '/src/components/componentStyling/EchoSideBar.css'
+import { useMainEchoStore } from '/src/stores/MainEchoStore.ts'
+
+const mainStore = useMainEchoStore()
+</script>
+
+<template>
+  <div class="side-bar-background">
+   
+  </div>
+</template>
