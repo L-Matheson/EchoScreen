5619ca8285bd259649db9e8efcfba8446400ab98
lmatheson2026 <144724348+lmatheson2026@users.noreply.github.com>
2025-05-07 21:55:48 -0400

    Dashboard work

--- EchoScreenFrontend/src/views/Dashboard.vue
+++ EchoScreenFrontend/src/views/Dashboard.vue
@@ -1,33 +1,36 @@
 <script lang="ts" setup>
 import { useMainEchoStore } from '../stores/MainEchoStore'
+import './styling/Dashboard.css' 
 
-// define the store
+
 const store = useMainEchoStore()
-console.log('home')
 </script>
 
 <template>
   <div class="p-0 m-0">
-csssssssssssssssssssssss
-    
+    <div class="top-cards">
+      <div class="single-card">
+        <i class="pi pi-chart-bar card-icon"></i>
+        <h2 class="card-title">Active Screens</h2>
+        <p class="card-content">0</p>
+      </div>
+      <div class="single-card">
+        <i class="pi pi-chart-line card-icon"></i>
+        <h2 class="card-title">Active Groups</h2>
+        <p class="card-content">0</p>
+      </div>
+      <div class="single-card">
+        <i class="pi pi-chart-pie card-icon"></i>
+        <h2 class="card-title">Card 3</h2>
+        <p class="card-content">Content for Card 3</p>
+      </div>
+      <div class="single-card">
+        <i class="pi pi-cog card-icon"></i>
+        <h2 class="card-title">Card 4</h2>
+        <p class="card-content">Content for Card 4</p>
+      </div>
+    </div>
   </div>
 </template>
 
-<style>
-
-.content {
-  border-radius: 25px 0px 0px 0px;
-  padding: 20px;
-  background-color: #f1f5f9;
-  min-height: 100vh - 70px;
-  width: 100%;
-  display: flex;
-  box-shadow: rgba(0, 0, 0, 0.06) 0px 2px 4px 0px inset;
-}
-.content-wrapper {
-  margin-top: 70px;
-  margin-left: 70px;
-  min-height: 100vh;
-  display: flex;
-}
-</style>
+<style></style>
